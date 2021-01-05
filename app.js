8945b133201f242af2a01c8fccfcefea3c2e9efd
michaldeshet <michaldror26@gmail.com>
2021-01-05 17:08:28 +0200

    controller + model TODo + connect to MongoBd

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const config = require('./config')
+const setupController = require('./controllers/setupController')
+const apiController = require('./controllers/apiController')
+
 const port = process.env.port || 3000
 
 app.use('/assest', express.static(__dirname + './public'))
 app.set('view engine', 'ejs')
+
+mongoose.connect(config.getDbConnectionString(), (err, res)=> {
+    if(err)
+    console.log(err);
+})
+setupController(app)
+apiController(app)
+
 app.listen(port)
